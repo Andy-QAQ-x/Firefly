ed19a8b48e3e0a9ebef3ae7d77c046ae5308f2e7
Andy-QAQ-x <1801395728@qq.com>
2026-07-09 17:18:32 +0800

    config:修改配置

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,13 +6,13 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "assets/images/a327e8f8a8a0c4649c6adef4ad028790.jpg",
 
 	// 名字
-	name: "Firefly",
+	name: "Andy",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "寻找旅途的意义",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
